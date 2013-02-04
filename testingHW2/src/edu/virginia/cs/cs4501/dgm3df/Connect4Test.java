ddd418a0a7b1713a2f6964a0ade77c7b02b3d026
Daniel Miller <dagrami@gmail.com>
2013-02-04 11:42:24 -0500

    junit

---
+++ testingHW2/src/edu/virginia/cs/cs4501/dgm3df/Connect4Test.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package edu.virginia.cs.cs4501.dgm3df;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author dgm3df
+ *
+ */
+public class Connect4Test {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
